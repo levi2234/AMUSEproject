d46ccb45db25890f134102005ea989ae301b7c09
Levi2234 <levi2234@hotmail.com>
2023-12-20 13:29:27 +0100

    cleanup

--- analysis_tools/collisiondetector.py
+++ analysis_tools/collisiondetector.py
@@ -1,125 +1,29 @@
-
-
-from scipy.spatial.transform import Rotation as R
 import numpy as np
+
+def closest_point_on_line(a, b, p):
+    """ Find the closest point on the line segment defined by points A and B to point P """
+    ap = p - a
+    ab = b - a
+    result = a + np.dot(ap, ab) / np.dot(ab, ab) * ab
+    return result
+
+def collision_detector(line_point1, line_point2, sphere_center, sphere_radius):
+    """ Check if the line defined by two points collides with the sphere """
+    closest_point = closest_point_on_line(np.array(line_point1), np.array(line_point2), np.array(sphere_center))
+    distance = np.linalg.norm(closest_point - np.array(sphere_center))
     
-def translate_and_rotate_to_x_axis(P1, P2):
-    """
-    Translate the line defined by points P1 and P2 to the origin and rotate it to align with the X-axis.
-    """
-    # Translate the points
-    P1_translated = np.array([0, 0, 0])
-    P2_translated = np.array(P2) - np.array(P1)
-
-    # Direction vector of the line
-    direction_vector = P2_translated / np.linalg.norm(P2_translated)
-
-    # Calculate the angles with the X, Y, Z axes
-    x_axis = np.array([1, 0, 0])
-    angle_with_x_axis = np.arccos(np.dot(direction_vector, x_axis))
-
-    # Rotation axis (cross product with x-axis)
-    rotation_axis = np.cross(direction_vector, x_axis)
-    rotation_axis = rotation_axis / np.linalg.norm(rotation_axis)
-
-    # Create rotation object
-    rotation = R.from_rotvec(rotation_axis * angle_with_x_axis)
-
-    # Rotate the line
-    P1_rotated = rotation.apply(P1_translated)
-    P2_rotated = rotation.apply(P2_translated)
-
-    return P1_rotated, P2_rotated
-
-def translate_and_rotate_point_to_x_axis(P, P1, P2):
-    """
-    Apply the same transformation used in translate_and_rotate_to_x_axis to a single point P.
-    """
-    # Translate the point
-    P1_translated = np.array([0, 0, 0])
-    P_translated = np.array(P) - np.array(P1)
-
-    # Reuse the calculation of direction vector, angle, and rotation from the line function
-    # Direction vector of the line
-    P2_translated = np.array(P2) - np.array(P1)
-    direction_vector = P2_translated / np.linalg.norm(P2_translated)
-
-    # Calculate the angles with the X, Y, Z axes
-    x_axis = np.array([1, 0, 0])
-
-    # Check if direction vector is parallel to the x-axis
-    if np.allclose(direction_vector, x_axis) or np.allclose(direction_vector, -x_axis):
-        # No rotation needed as the line is already aligned with the x-axis
-        return P_translated
-
-    angle_with_x_axis = np.arccos(np.dot(direction_vector, x_axis))
-
-    # Rotation axis (cross product with x-axis)
-    rotation_axis = np.cross(direction_vector, x_axis)
-
-    # Check if rotation axis is zero vector
-    if not np.any(rotation_axis):
-        # This means the line is parallel to the x-axis, and no rotation is needed
-        return P_translated
-
-    rotation_axis = rotation_axis / np.linalg.norm(rotation_axis)
-
-    # Create rotation object
-    rotation = R.from_rotvec(rotation_axis * angle_with_x_axis)
-    P1_rotated = rotation.apply(P1_translated)
-    P2_rotated = rotation.apply(P2_translated)
-
-    # Rotate the point
-    P_rotated = rotation.apply(P_translated)
-    P1_rotated = rotation.apply(P1_translated)
-    P2_rotated = rotation.apply(P2_translated)
-    return P_rotated, P1_rotated, P2_rotated
-
-def collision_detector(P1,P2, P, P_radius):
-    P_rotated, P1_rotated, P2_rotated = translate_and_rotate_point_to_x_axis(P1, P2, P)
-    # print(P_rotated)
-    # print(P1_rotated)
-    # print(P2_rotated)
-    # print("Distance to line: ", np.linalg.norm(P_rotated[1:]))
-    if (P_rotated[0] < 0) or (P_rotated[0] > P2_rotated[0]): 
+    #check if the sphere is beyond the line segment
+    if np.dot(closest_point - np.array(line_point1), closest_point - np.array(line_point2)) > 0:
         return False
-    if np.linalg.norm(P_rotated[1:]) <= P_radius: #here we check the y and z coordinates of the 
-        return True
     else:
-        return False
-    
+        
+         return distance <= sphere_radius
 
-#uncomment the following lines to test the functions
+# # Example usage:
+# line_point1 = [-3, 0, 0]
+# line_point2 = [-0.5, 0, 0]
+# sphere_center = [0, 1, 0]
+# sphere_radius = 1
 
-# # Example usage
-# P = [0, 4,1]  # Example point
-# P1 = [0, 1, 0]  # Line start
-# P2 = [0, 3, 0]  # Line end
-# transformed_point = translate_and_rotate_point_to_x_axis(P, P1, P2)
-# transformed_P1, transformed_P2 = translate_and_rotate_to_x_axis(P1, P2)
-
-# print(collision_detector(P, P2, P1, 1))
-
-# #plotting
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D
-# import numpy as np
-
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-
-# # Plot the line
-# ax.plot([P1[0], P2[0]], [P1[1], P2[1]], [P1[2], P2[2]], color='k')
-
-# # Plot the point
-# ax.scatter(P[0], P[1], P[2], color='r')
-
-# # Plot the transformed line
-
-# ax.plot([transformed_P1[0], transformed_P2[0]], [transformed_P1[1], transformed_P2[1]], [transformed_P1[2], transformed_P2[2]], color='b')
-
-# # Plot the transformed point
-
-# ax.scatter(transformed_point[0], transformed_point[1], transformed_point[2], color='g')
-
-# plt.savefig("transformation.png")
+# collision = collision_detector(line_point1, line_point2, sphere_center, sphere_radius)
+# print("Collision:", collision)
---
+++ run_simulation_rocky.py
@@ -0,0 +1,210 @@
+from analysis_tools.plot_system import systemplotter
+from analysis_tools.animation import Animator
+
+from simulation_tools import create_planet_and_atmosphere as CPA
+from simulation_tools.profiles import atmospheric_profiles as AP
+from amuse.ext.orbital_elements import new_binary_from_orbital_elements
+from amuse.community.fi.interface import Fi
+from amuse.community.bhtree.interface import BHTree
+from amuse.couple.bridge import Bridge
+from amuse.lab import nbody_system
+from amuse.units import units
+from amuse.units import constants
+from amuse.io import write_set_to_file
+from amuse.datamodel import Particles
+
+import os
+import sys
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+# model evolution
+timestep = 1 | units.hour
+simulation_duration = 5 | units.day
+
+# ----------------------CREATE THE PLANET----------------------
+
+# for now create the model with planet_to_sph,
+# can only run after the profiles are complete in earth_profile/PREM.csv
+
+planet_mass = 1 | units.MEarth
+planet_radius = 1 | units.REarth
+atmosphere_profile = AP.true_profile
+atmosphere_height = 100|units.km
+atmosphere_density = 1.225 | units.kg * units.m** (-3)
+num_points = 50000
+gamma = 1.4
+
+# Check if the correct number of command-line arguments are provided
+if len(sys.argv) != 2:
+    print("Usage: python my_script.py integer_argument")
+    sys.exit(1)
+
+# Extract the integer argument from the command line
+try:
+    arg1 = int(sys.argv[1])
+except ValueError:
+    print("Error: Argument must be an integer.")
+    sys.exit(1)
+atmosphere_speed = int(sys.argv[1])
+print("Speed:", atmosphere_speed)
+
+planet, atmosphere = CPA.create_planet_and_atmosphere(planet_mass=planet_mass , 
+                                                planet_radius= planet_radius, 
+                                                atmosphere_profile=atmosphere_profile, 
+                                                atmosphere_height= atmosphere_height, 
+                                                atmosphere_density=atmosphere_density, 
+                                                num_points=num_points,
+                                                gamma=gamma)
+
+
+#adding radial velocities to atmosphere
+#get x,y,z positions and normalize
+x = atmosphere.x.value_in(units.m)
+y = atmosphere.y.value_in(units.m)
+z = atmosphere.z.value_in(units.m)
+r = np.sqrt(x**2 + y**2 + z**2)
+x = x/r
+y = y/r
+z = z/r
+
+#set velocities
+
+vx = x * atmosphere_speed + np.random.uniform(-10,10) | units.m / units.s
+vy = y * atmosphere_speed + np.random.uniform(-10,10)| units.m / units.s
+vz = z * atmosphere_speed + np.random.uniform(-10,10)| units.m / units.s
+atmosphere.vx = vx 
+atmosphere.vy = vy
+atmosphere.vz = vz
+
+
+#---------------------Create a Moon --------------------
+#calcualte the planets total mass
+planet_mass = planet.mass.sum()+ atmosphere.mass.sum()
+print('planet mass:', planet_mass.in_(units.MEarth))
+
+# moon_mass = 0.008 | units.MEarth # mass Europa, previously: 0.012*planet_mass
+moon_mass = 0.012 | units.MEarth #
+# distance_planet_moon = 671000 | units.km # distance Europa from jupiter
+distance_planet_moon = 621600 | units.km # distance Io and Jupiter
+eccentricity_moon = 0 # 0.009 for Europa, 0.004 for Io, but close enough to 0.
+#create binary system with the planet and the moon from orbital elements
+
+system = new_binary_from_orbital_elements(planet_mass, moon_mass, distance_planet_moon, eccentricity_moon, G = constants.G)
+
+#move most massive object to 0,0,0
+system.position = system.position -system[0].position
+
+#add moon to own particle set (could be done better but this only works for 2 objects)
+moon = Particles(1)
+moon.mass = moon_mass
+moon.position = system[1].position
+moon.velocity = system[1].velocity
+
+
+#add sun from geocentric frame
+system = new_binary_from_orbital_elements(planet_mass, 1 | units.MSun, 1| units.AU, 0, G = constants.G)
+#center around earth
+system.position = system.position -system[0].position
+#add sun to own particle set
+sun = Particles(1)
+sun.mass = 1 | units.MSun
+sun.position = system[1].position
+sun.velocity = system[1].velocity
+
+# #----------------------BEFORE PLOT-----------------------
+fig, ax = plt.subplots()
+planet_radius_temp = planet.radius.value_in(units.m)
+circle = plt.Circle((0, 0), planet_radius_temp[0], color='green',alpha= 0.2, fill=True)
+ax.scatter(atmosphere.x.value_in(units.m), atmosphere.y.value_in(units.m), s=0.01)
+ax.add_artist(circle)
+ax.set_aspect('equal')
+ax.set_xlabel('x (m)')
+ax.set_ylabel('y (m)')
+ax.set_title('Planet and Atmosphere before simulation \n (Earth Atmosphere Distribution)')
+plt.savefig('simulation_results/planetbefore.png')
+
+#----------------------PHYSICS SETUP----------------------
+
+# add the hydroparticles to a hydrocode and run for some time to check if it's stable
+
+converter = nbody_system.nbody_to_si(1e24 | units.kg, 6.371e6 | units.m)
+
+#setup hydro code
+hydro_code = Fi(converter, mode='openmp', redirection='none')
+hydro_code.parameters.epsilon_squared = planet_radius**2
+hydro_code.parameters.n_smooth_tol = 0.01
+hydro_code.gas_particles.add_particles(atmosphere)
+hydro_code.dm_particles.add_particle(planet)
+
+#setup gravity code
+gravity_code = BHTree(converter)
+gravity_code.parameters.epsilon_squared = planet_radius**2
+gravity_code.particles.add_particles(moon)
+gravity_code.particles.add_particles(sun)
+
+#setup the bridge
+bridge = Bridge(use_threading=False)
+bridge.add_system(gravity_code, (hydro_code,)) #makes sure planet/atmosphere particles are affected by gravity
+bridge.add_system(hydro_code, (gravity_code,)) #makes sure moon is also affected by hydrodynamics and stays in orbit
+
+# Set the timestep for the bridge
+bridge.timestep = timestep
+
+
+
+path_results = f'simulation_results/Earth_planet_{atmosphere_speed}ms/'
+if not os.path.exists(path_results):
+    os.mkdir(path_results)
+
+index = 0
+triggered_injection = False
+
+# #----------------------BEFORE PLOT-----------------------
+# systemplotter(hydro_code.gas_particles, xlabel='x', ylabel='y').plot(c="b",s=0.01, close=False)
+# systemplotter(gravity_code.particles, xlabel='x', ylabel='y').plot( c="r",s=40, close=False)
+# systemplotter(hydro_code.dm_particles, xlabel='x', ylabel='y').plot(save="simulation_results/testplanetBefore.png", c="r",s=20)
+
+try:
+    previous_time = None
+    #----------------------RUN THE SIMULATION----------------------
+    while (hydro_code.model_time < simulation_duration):
+        if previous_time == None:
+            previous_time = hydro_code.model_time.value_in(units.hour)
+        #hydro_code.evolve_model(hydro_code.model_time + bridge.timestep)
+        bridge.evolve_model(hydro_code.model_time + bridge.timestep)
+        if previous_time != hydro_code.model_time.value_in(units.hour):
+            previous_time = hydro_code.model_time.value_in(units.hour)
+        else:
+            print("Didn't evolve")
+            break
+        print('Time=', hydro_code.model_time.in_(units.hour))
+
+        write_set_to_file(hydro_code.gas_particles, path_results+f'gas_particles_{index}.hdf5', overwrite_file=True)
+        write_set_to_file(hydro_code.dm_particles, path_results+f'dm_particles_{index}.hdf5', overwrite_file=True)
+        write_set_to_file(gravity_code.particles, path_results+f'gravity_particles_{index}.hdf5', overwrite_file=True)
+    
+        index += 1
+except Exception as e:
+    print("Speed failed:", atmosphere_speed)
+    pass
+
+
+# #----------------------AFTER PLOT-----------------------
+# systemplotter(hydro_code.gas_particles, xlabel='x', ylabel='y').plot(save=f"simulation_results/endplot_{atmosphere_speed}.png", c="b",s=0.01, close=False)
+# systemplotter(gravity_code.particles, xlabel='x', ylabel='y').plot(save="simulation_results/endplot.png", c="r",s=40, close=False)
+# systemplotter(hydro_code.dm_particles, xlabel='x', ylabel='y').plot(save=f"simulation_results/endplot_{atmosphere_speed}.png", c="r",s=20)
+
+
+#----------------------STOP THE SIMULATION-----------------------
+hydro_code.stop()
+gravity_code.stop()
+
+
+# path = f'simulation_results/Earth_planet_{atmosphere_speed}ms/'
+# animator = Animator(path, xlabel='x', ylabel='y', xlim=0.005, ylim=0.005, s_atmosphere=0.01)
+# animator.make_animation(save_path=f'simulation_results/Earth_planet_{atmosphere_speed}.mp4')
+
+
+
